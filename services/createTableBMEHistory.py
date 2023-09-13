f7337170c79e1b86f4cf3299d08196fcf5eb9429
MarinaOl <missisololol@gmail.com>
2023-09-13 17:50:01 +0300

    Updated history table

--- services/createTableBMEHistory.py
+++ services/createTableBMEHistory.py
@@ -1,23 +1,27 @@
-# import os
+import os
+from dotenv import load_dotenv
 import mysql.connector as database
 
-# username = os.environ.get("username")
-# password = os.environ.get("password")
+load_dotenv()
+
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DB = os.getenv('MYSQL_DB')
 
 connection = database.connect(
-    user='marina',
-    password='ma1ri2na3',
+    user=MYSQL_USER,
+    password=MYSQL_PASSWORD,
     host='localhost',
-    database="meteo")
+    database=MYSQL_DB)
 
 cursor = connection.cursor()
 
 def create_table_bmehistorytemerature():
     try:
-        statement = "CREATE OR REPLACE TABLE bme_temerature_history (id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, date DATE, min_temperature DECIMAL(4,2), max_temperature DECIMAL(4,2))"
+        statement = "CREATE OR REPLACE TABLE bme_history (id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, date DATE, min_temperature DECIMAL(4,2), max_temperature DECIMAL(4,2), min_humidity DECIMAL(4,2), max_humidity DECIMAL(4,2), min_pressure DECIMAL(4,2), max_pressure DECIMAL(4,2))"
         cursor.execute(statement)
         connection.commit()
-        print("Successfully created table bme_temerature_history")
+        print("Successfully created table bme_history")
     except database.Error as e:
         print(f"Error adding entry to database: {e}")
 
